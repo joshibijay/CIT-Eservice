11b7a6ec60cf4966fe3cbd6d34c29b1ed29dcf80
joshibijay <159009661+joshibijay@users.noreply.github.com>
2024-08-15 15:43:04 +0545

    POM change

--- pages/test.js
+++ pages/test.js
@@ -10,7 +10,7 @@ const mobileno='98'+faker.string.numeric(8);
 const telno='01'+faker.string.numeric(7);
 const address=faker.location.city();
 const accountno=faker.string.numeric({min:20});
-const panno=faker.number.int({min: 100000000, max: 999999999});
+const panno=faker.string({min: 100000000, max: 999999999});
 const identityno=faker.string.numeric({min:5})+ '/' +'-'+faker.string.numeric({max:100});
 
 module.exports ={
